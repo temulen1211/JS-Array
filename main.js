481f5038f63d396c24d0cb4b381d2d3780189ac9
Evening <evening@admins-iMac-9.local>
2023-02-02 16:31:58 +0800

    js array

---
+++ main.js
@@ -0,0 +1,29 @@
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+
+console.log(itCompanies);
+
+console.log(itCompanies.length);
+
+console.log(itCompanies[0]);
+console.log(itCompanies[Math.floor(itCompanies.length/2)]);
+console.log(itCompanies[itCompanies.length-1]);
+
+
+console.log(itCompanies[0]);
+console.log(itCompanies[1]);
+console.log(itCompanies[2]);
+console.log(itCompanies[3]);
+console.log(itCompanies[4]);
+console.log(itCompanies[5]);
+console.log(itCompanies[6]);
+
+console.log(itCompanies[0].toUpperCase());
+console.log(itCompanies[1].toUpperCase());
+console.log(itCompanies[2].toUpperCase());
+console.log(itCompanies[3].toUpperCase());
+console.log(itCompanies[4].toUpperCase());
+console.log(itCompanies[5].toUpperCase());
+console.log(itCompanies[6].toUpperCase());
+
+console.log(`${itCompanies[0]}, ${itCompanies[1]}, ${itCompanies[2]}, ${itCompanies[3]}, ${itCompanies[4]}, ${itCompanies[5]}, ${itCompanies[6]} zereg medeelliin technologiin tomoohon companiud.`);
+
